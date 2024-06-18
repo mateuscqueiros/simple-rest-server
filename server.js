1f9dae58c266749528ed1846baebba024cbd2db1
Mateus Queirós <mateuscqueiros@gmail.com>
2024-06-17 22:53:53 -0300

    update data

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-var express = require("express");
-var jsonServer = require("json-server");
-var dotenv = require("dotenv");
-var auth = require("json-server-auth");
-var cors = require("cors");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
-var server = express();
-server.use(cors());
+require("dotenv").config();
 
-dotenv.config({ path: "./.env" });
-var router = jsonServer.router("db.json");
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  }),
+);
+server.options("*", cors());
 
-server.db = router.db;
-
-server.use(auth);
+server.use(middlewares);
 server.use(router);
-
-var port = process.env.PORT;
-
-server.listen(port, () => {
-  console.log("Server running on port", port);
+server.listen(process.env.PORT, () => {
+  console.log("JSON Server is running");
 });
